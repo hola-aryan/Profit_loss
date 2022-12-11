fc75f3e8afe2b491db1c53f2c2864436e990072a
hola-aryan <aryanisastudent@gmail.com>
2022-12-12 01:33:55 +0530

    feat: done accept style

---
+++ index.js
@@ -0,0 +1,44 @@
+// Step 1 - selecting all the elements
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
+
+// Ex-06
+submitBtn.addEventListener("click", submitHandler);
+
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stocksQuantity.value);
+  var curr = Number(currentPrice.value);
+
+  calculateProfitAndLoss(ip, qty, curr);
+}
+
+// Ex-05
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > current) {
+    var loss = (initial - current) * quantity;
+    var lossPercentage = (loss / initial) * 100;
+
+    showOutput(
+      `Hey, the loss is ${loss} and the percent is ${lossPercentage}%`
+    );
+    outputBox.style.color = 'Red';
+  } else if (current > initial) {
+    var profit = (current - initial) * quantity;
+    var profitPercentage = (profit / initial) * 100;
+
+    showOutput(
+      `Hey, the profit is ${profit} and the percent is ${profitPercentage}%`
+    );
+    outputBox.style.color = 'green';
+  } else {
+    showOutput(`No pain no gain and no gain no pain`);
+  }
+}
+
+function showOutput(message) {
+  outputBox.innerHTML = message;
+}
